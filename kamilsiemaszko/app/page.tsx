84360c2f5741d0b58e6306be7ee1c2267ed7076f
Adrian Brodziński <adrbrodz@gmail.com>
2025-08-18 16:29:16 +0200

    font

--- kamilsiemaszko/app/page.tsx
+++ kamilsiemaszko/app/page.tsx
@@ -5,9 +5,17 @@ import Contact from "./components/contact/contact";
 import FAQ from "./components/faq/faq";
 import TrustindexWidget from "./components/trustindexWidget";
 
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+
 export default function Home() {
   return (
-    <div className="main-container">
+    <div className={`${montserrat.className} main-container`}>
       <StickyHeader />
       <HeroPage />
       <Content />
