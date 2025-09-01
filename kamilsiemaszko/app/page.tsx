755b7634f1bba84d7d48fc82ad9b638a1be1ef38
Adrian Brodziński <adrbrodz@gmail.com>
2025-09-01 10:51:43 +0200

    widget

--- kamilsiemaszko/app/page.tsx
+++ kamilsiemaszko/app/page.tsx
@@ -1,10 +1,10 @@
+'use client'
 import StickyHeader from "./components/sticky-header/stickyHeader";
 import HeroPage from "./components/heroPage";
 import Content from "./components/content/content";
 import Contact from "./components/contact/contact";
 import FAQ from "./components/faq/faq";
-import TrustindexWidget from "./components/trustindexWidget";
-
+import { ElfsightWidget } from "react-elfsight-widget";
 import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
@@ -19,7 +19,7 @@ export default function Home() {
       <StickyHeader />
       <HeroPage />
       <Content />
-      <TrustindexWidget />
+      <ElfsightWidget widgetId="6da72076-585c-4b1e-963d-a332ac6ed0a7"/>
       <Contact />
       <FAQ />
       <p className="italic text-xs text-right text-white p-5 px-10">
